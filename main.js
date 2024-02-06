4e5790921c9e6078cbc1b624a4bafbae362ef4b9
Sunil Kakarlapudi <sunilkumarkakarlapudi@gmail.com>
2024-02-06 20:55:57 +0530

    First push

---
+++ main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron')
+
+// The list of open windows
+let windows = new Set()
+
+// Function to create the main window
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  })
+  win.loadFile('index.html')
+  windows.add(win)
+}
+
+// Function to create a new window
+const createNewWindow = () => {
+  const window = new BrowserWindow({
+    width: 900,
+    height: 600
+  });
+  window.loadFile('priceladder.html')
+  windows.add(window);
+}
+
+
+// App init
+app.whenReady().then(() => {
+  createWindow()
+})
+
+ipcMain.on('update-value', function (event, arg) {
+  console.log(arg);
+   
+  // Passing the data from Main Process to index.html
+  // BrowserWindow Instance, value will be received in 
+  // the index.js based on the Key set here. Using the 
+  // 'win.webContents.send' method for Asynchronous Data Transfer
+  win.webContents.send('updateValue', arg);
+});
